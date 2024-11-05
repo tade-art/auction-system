c82c0aedae9557e117ea92fa4dd0816e90931485
Tadas Ivanauskas <tivanauskas32@gmail.com>
2024-11-05 11:31:40 +0000

    need to finish client logic and test against auto marker

--- Client.java
+++ Client.java
@@ -3,18 +3,31 @@ import java.rmi.registry.Registry;
 
 public class Client {
   public static void main(String[] args) {
-    int n = Integer.parseInt(args[0]);
 
     try {
       String name = "Auction";
       Registry registry = LocateRegistry.getRegistry("localhost");
       Auction server = (Auction) registry.lookup(name);
 
-      AuctionItem toReturn = server.getSpec(n);
-      System.out.println("object returned: " + toReturn.itemID + "\nObject Name:" + toReturn.name
-          + "\nObject Description:" + toReturn.description + "\nObject highest bid:" + toReturn.highestBid);
+      // FIGURE OUT A WAY TO GENERATE NEW AUCTION, KEEP IT CLEAN AND TIDY
+      switch (args[0]) {
+        case "new":
+          // server.newAuction(args[1], server.generateAuctionSaleItem((args[1]), args[2],
+          // Integer.parseInt(args[3])));
+        case "close":
+          server.closeAuction(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        case "bid":
+          server.bid(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+        case "list":
+          server.listItems();
+        case "register":
+          server.register(args[1]);
+      }
 
-    } catch (Exception e) {
+      // AuctionItem toReturn = server.getSpec(n);
+    }
+
+    catch (Exception e) {
       System.err.println("Exception:");
       e.printStackTrace();
     }
--- Server.java
+++ Server.java
@@ -4,7 +4,6 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class Server implements Auction {
     private ArrayList<AuctionItem> items = new ArrayList<AuctionItem>();
@@ -36,6 +35,21 @@ public class Server implements Auction {
         item.description = description;
         item.highestBid = highestBid;
         return item;
+    }
+
+    public AuctionResult generateAuctionResult(String email, int winningBid) {
+        AuctionResult toReturn = new AuctionResult();
+        toReturn.winningEmail = email;
+        toReturn.winningPrice = winningBid;
+        return toReturn;
+    }
+
+    public AuctionSaleItem generateAuctionSaleItem(String name, String description, int reservePrice) {
+        AuctionSaleItem toReturn = new AuctionSaleItem();
+        toReturn.name = name;
+        toReturn.description = description;
+        toReturn.reservePrice = reservePrice;
+        return toReturn;
     }
 
     @Override
@@ -75,7 +89,29 @@ public class Server implements Auction {
     @Override
     public int newAuction(int userID, AuctionSaleItem item) throws RemoteException {
         try {
-        } catch (Exception e) {
+            if (!userIDList.containsKey(userID) || item == null)
+                return -1;
+
+            int auctionID = 0;
+
+            boolean exists = true;
+            while (auctionID == 0 || exists) {
+                auctionID = (int) (Math.random() * 10000);
+                exists = false;
+                for (AuctionItem i : items) {
+                    if (i.itemID == auctionID) {
+                        exists = true;
+                        break;
+                    }
+                }
+            }
+
+            AuctionItem auctionItem = generateItem(auctionID, item.name, item.description, item.reservePrice);
+            items.add(auctionItem);
+            return auctionID;
+        }
+
+        catch (Exception e) {
             System.err.println("Error with newAuction:");
             e.printStackTrace();
         }
@@ -85,7 +121,10 @@ public class Server implements Auction {
     @Override
     public AuctionItem[] listItems() throws RemoteException {
         try {
-        } catch (Exception e) {
+            return (AuctionItem[]) items.toArray();
+        }
+
+        catch (Exception e) {
             System.err.println("Error in listItems:");
             e.printStackTrace();
         }
@@ -95,7 +134,21 @@ public class Server implements Auction {
     @Override
     public AuctionResult closeAuction(int userID, int itemID) throws RemoteException {
         try {
-        } catch (Exception e) {
+            if (!userIDList.containsKey(userID))
+                return null;
+
+            AuctionItem item = null;
+            for (AuctionItem i : items)
+                if (i.itemID == itemID) {
+                    item = i;
+                    break;
+                }
+
+            items.remove(item);
+            return generateAuctionResult(userIDList.get(userID), item.highestBid);
+        }
+
+        catch (Exception e) {
             System.err.println("Error in closeAuction:");
             e.printStackTrace();
         }
@@ -105,7 +158,24 @@ public class Server implements Auction {
     @Override
     public boolean bid(int userID, int itemID, int price) throws RemoteException {
         try {
-        } catch (Exception e) {
+            if (!userIDList.containsKey(userID))
+                return false;
+
+            AuctionItem item = null;
+            for (AuctionItem i : items)
+                if (i.itemID == itemID) {
+                    item = i;
+                    break;
+                }
+
+            if (item == null || price <= item.highestBid || price < 0)
+                return false;
+
+            item.highestBid = price;
+            return true;
+        }
+
+        catch (Exception e) {
             System.err.println("Error in bid:");
             e.printStackTrace();
         }
